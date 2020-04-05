d1ba82b99dda83c203b2ed7d7cf77b500d8649e7
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2020-04-05 21:35:12 +0530

    initial commit

---
+++ lib/ui/screen/rajashekar.dart
@@ -0,0 +1,16 @@
+/*
+ * @Author GS
+ */
+import 'package:flutter/material.dart';
+
+class RajaShekar extends StatefulWidget {
+  @override
+  _RajaShekarState createState() => _RajaShekarState();
+}
+
+class _RajaShekarState extends State<RajaShekar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
