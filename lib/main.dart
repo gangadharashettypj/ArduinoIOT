68edc46d22a864451a1e0c8b12fd58fa035e86c2
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2020-05-10 20:04:11 +0530

    resolved bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,34 @@
-import 'package:arduinoiot/resources/nestbees_resources.dart';
-import 'package:arduinoiot/service/server/server.dart';
-import 'package:arduinoiot/ui/screen/builder/builder.dart';
-import 'package:arduinoiot/ui/screen/feature/car/car_controller.dart';
-import 'package:arduinoiot/ui/screen/feature/chat/chat.dart';
-import 'package:arduinoiot/ui/screen/feature/joystick/joystick.dart';
-import 'package:arduinoiot/ui/screen/feature/servo/servo.dart';
-import 'package:arduinoiot/ui/screen/home/home.dart';
-import 'package:arduinoiot/ui/screen/home/trialer.dart';
-import 'package:arduinoiot/ui/screen/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-  BuildContext _context;
   @override
   Widget build(BuildContext context) {
-    _context = context;
-    listenForNotifications();
-    SystemChrome.setEnabledSystemUIOverlays([]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('app bar'),
       ),
-      title: "NestBees",
-      home: SplashScreen(),
-      routes: <String, WidgetBuilder>{
-        R.routes.splash: (BuildContext context) => SplashScreen(),
-        R.routes.home: (BuildContext context) => Home(),
-        R.routes.trial: (BuildContext context) => Trail(),
-        R.routes.car: (BuildContext context) => CarController(),
-        R.routes.chat: (BuildContext context) => Chat(),
-        R.routes.joystick: (BuildContext context) => JoyStick(),
-        R.routes.builder: (BuildContext context) => ScreenBuilder(),
-        R.routes.servo: (BuildContext context) => Servo(
-              max: 1000,
-              min: 0,
-              prefix: 's',
-              suffix: '>>',
-              onChange: (val) {
-                print(val);
-              },
+      body: Container(
+        width: double.infinity,
+        child: Row(
+          children: <Widget>[
+            Text('sdgf'),
+            RaisedButton(
+              onPressed: () {},
             ),
-      },
+            RaisedButton(
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  void listenForNotifications() async {
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS = IOSInitializationSettings(
-      requestSoundPermission: false,
-      requestBadgePermission: false,
-      requestAlertPermission: false,
-      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
-    );
-    var initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: onNotificationSelected);
-
-    Server().registerService(R.api.notification,
-        (Map<String, String> response) {
-      raiseNotification(response['notificationMessage'],
-          response['notificationBody'], response['notificationTitle']);
-    });
-  }
-
-  Future onNotificationSelected(String payload) async {
-    if (payload != null) {
-      debugPrint('notification payload: ' + payload);
-    }
-    await Navigator.pushNamed(
-      _context,
-      '',
-    );
-  }
-
-  void raiseNotification(String msg, String body, String title) async {
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'arduino_iot_channel', 'arduino_iot', 'A notification for iot devices',
-        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin
-        .show(0, title, body, platformChannelSpecifics, payload: 'msg');
-  }
-
-  Future onDidReceiveLocalNotification(
-      int id, String title, String body, String payload) {}
 }
