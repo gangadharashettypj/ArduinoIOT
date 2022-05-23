c19cba21c7d15d1b29bae031c006970d28a47d08
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2022-05-23 20:22:27 +0530

    basic template working fine

--- arduino/bpm_nano/bpm_nano.ino
+++ arduino/bpm_nano/bpm_nano.ino
@@ -5,7 +5,6 @@ unsigned long millisBefore;
 // the setup routine runs once when you press reset:
 void setup()
 {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
  
